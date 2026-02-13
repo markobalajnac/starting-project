a537bdf211d916f3c8b6367d7eef0c5279c3a6e9
Marko Djordjevic <djomarko97@gmail.com>
2026-02-13 16:55:34 +0100

    feat(): Output list content dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,14 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            /> */}
           </ul>
         </section>
         <section id="examples">
